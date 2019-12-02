5a0ca8909149fefa64d220d170ab77f3ca37f814
Arttu Salmijärvi <arasal@utu.fi>
2019-12-02 13:08:39 +0200

    Korjattu taas frontin ongelmia

---
+++ build/precache-manifest.964d9c057cf581fb591b2d17e2e2c416.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbf2a8a9460970e8cf8d1d7789d1e5a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e727e824b906e9bcbb5a",
+    "url": "/static/css/main.14e238f1.chunk.css"
+  },
+  {
+    "revision": "4a77e414eb919c2e3267",
+    "url": "/static/js/2.935c9421.chunk.js"
+  },
+  {
+    "revision": "e727e824b906e9bcbb5a",
+    "url": "/static/js/main.717c25b4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
