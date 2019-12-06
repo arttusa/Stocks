0ce436385bb605f114ea032bd1fe4bc74e307a26
Arttu Salmijärvi <arasal@utu.fi>
2019-12-06 23:02:44 +0200

    Uusi build ja frontti lisätty repoon

---
+++ build/precache-manifest.7a0dd18754eb4e85809dbe5604d8f918.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "223c20da90a4286653c5ddeddca5335b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "901c390975a0182eab79",
+    "url": "/static/css/main.213ceba6.chunk.css"
+  },
+  {
+    "revision": "0fcaef19e41b71a969e8",
+    "url": "/static/js/2.bba109de.chunk.js"
+  },
+  {
+    "revision": "901c390975a0182eab79",
+    "url": "/static/js/main.07b5ffd2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.975562c04e8d98284ad232053bb4e757.js"
+  "/precache-manifest.7a0dd18754eb4e85809dbe5604d8f918.js"
 );
 
 self.addEventListener('message', (event) => {
